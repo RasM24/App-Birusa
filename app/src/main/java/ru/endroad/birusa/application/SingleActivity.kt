f32ae8a9cabb615fdcf3b04a895d7af412676e23
RasM24 <dvolyatik@yandex.ru>
2019-12-18 22:47:33 +0700

    [rebirth] Создал SingleActivity

---
+++ app/src/main/java/ru/endroad/birusa/application/SingleActivity.kt
@@ -0,0 +1,14 @@
+package ru.endroad.birusa.application
+
+import endroad.birusa.R
+import ru.endroad.arena.viewlayer.activity.BaseActivity
+
+class SingleActivity : BaseActivity() {
+
+	override val layout = R.layout.activity_main
+
+	override val theme = R.style.AppTheme
+
+	override fun onFirstCreate() {
+	}
+}
